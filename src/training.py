85f22c9f2d63b99c671b01ea8bec6993cc71baea
unknown <prisoner.sedwna@gmail.com>
2023-09-10 19:54:13 +0330

    requirement

--- src/training.py
+++ src/training.py
@@ -19,73 +19,57 @@ def trainer(json_name):
     words = []
     classes = []
     documents = []
-    ignore_letters = ['?', '!', ',', '.', '؟']
 
     for intent in intents['intents']:
         for pattern in intent["patterns"]:
-            # pattern = nlp.normalizer_(pattern)
-            # word_list = nlp.tokenizer_(pattern)
-            # print(word_list)
             word_list = nlp.clean_up_sentence(pattern)
             words.extend(word_list)
             documents.append((word_list, intent["tag"]))
             if intent["tag"] not in classes:
                 classes.append(intent['tag'])
 
-    words = [word for word in words if word not in ignore_letters]  # lemmatizer(word)
-
     print(words)
-    # print(len(words))
-    # print(classes)
-    # print(len(classes))
 
     words = sorted(set(words))
     classes = sorted(set(classes))
     pickle.dump(words, open('../pkl_file/words.pkl', 'wb'))
     pickle.dump(classes, open('../pkl_file/classes.pkl', 'wb'))
-    # print(words)
 
     training = []
-    output_empty = [0] * len(classes)
-
-    # print(output_empty)
-    # print(documents)
+    output_empty = [0] * len(classes)  # create zero array, count == number of tag in json file
 
     for document in documents:
         bag = []
 
-        word_patterns = document[0]
-        # word_patterns = [word for word in words if word not in ignore_letters]  # lemmatizer(word)
-        # print(word_patterns)
-        # print(len(word_patterns))
+        word_patterns = document[0]  # document[0] == word & document[1] == tag
+
         for word in words:
+            # create a matrix by 0,1 value with all of words was in json file patterns
             bag.append(1) if word in word_patterns else bag.append(0)
+        # set matrix tag
         output_row = list(output_empty)
         output_row[classes.index(document[1])] = 1
+
+        # bag == 0,1 words matrix & output_row == 0,1 tags of words
         training.append([bag, output_row])
-    # print(training)
-    # print("-----------------------")
+
     random.shuffle(training)
-    # print(training)
+
     training = np.array(training)
 
-    train_x = list(training[:, 0])
-    train_y = list(training[:, 1])
-
-    # print(train_x)
-    # print(train_y)
-    # print(len(train_x))
-    # print(len(train_y))
-
+    train_x = list(training[:, 0])  # feature
+    train_y = list(training[:, 1])  # label
+    print("train", train_x)
+    print("len", len(train_x[0]))
     model = Sequential()
-    model.add(Dense(128, input_shape=(len(train_x[0]),), activation="relu"))
+    model.add(Dense(128, input_shape=(len(train_x[0]),), activation="relu"))  # input layer
     model.add(Dropout(0.5))
     model.add(Dense(64, activation="relu"))
     model.add(Dropout(0.5))
-    model.add(Dense(len(train_y[0]), activation='softmax'))
+    model.add(Dense(len(train_y[0]), activation='softmax'))  # output layer
 
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
 
     hist = model.fit(np.array(train_x), np.array(train_y), epochs=20, batch_size=128, verbose=1)
     model.save('../chat_bot_model/chatbotmodel.h5', hist)
