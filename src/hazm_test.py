1fa9330f53f03b517a7200323d76cbe97c47de99
unknown <prisoner.sedwna@gmail.com>
2023-09-11 12:24:16 +0330

    Update hazm_test.py

--- src/hazm_test.py
+++ src/hazm_test.py
@@ -1,9 +1,7 @@
 import hazm
-sent2vec_model_path = 'sent2vec.model'
 
 
-wordEmbedding = hazm.WordEmbedding(model_type='fasttext')
-wordEmbedding.load_model('word2vec.bin')
-# wordEmbedding.doesnt_match(['سلام', 'درود', 'خداحافظ', 'پنجره'])
-#
-# wordEmbedding.doesnt_match(['ساعت', 'پلنگ', 'شیر'])
+tokenizer = hazm.SentenceTokenizer()
+lis = tokenizer.tokenize('سلام من اومدم علی رسیده؟؟ باید چیکار کنم؟')
+print(lis)
+
